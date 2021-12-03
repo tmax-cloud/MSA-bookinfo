2339be10e5ff14f06faf20b180da83f8064f1169
S-hayeon <hayeona92@gmail.com>
2021-12-03 13:07:39 +0900

    [feat] add swagger api

---
+++ src/main/java/com/example/bookinfo/model/Book.java
@@ -0,0 +1,29 @@
+package com.example.bookinfo.model;
+
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private Integer quantity;
+
+    public Book() {}
+
+    public Book(String title, int quantity) {
+        this.title = title;
+        this.quantity = quantity;
+    }
+
+
+}
+
